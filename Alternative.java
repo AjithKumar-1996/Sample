98065067cff4f696d17587eac211d2a51fa08464
Ajith Kumar <ajithkumar.r1996@gmail.com>
2022-02-15 10:40:18 +0200

    Testing

---
+++ Alternative.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Alternative {
+    public static void main(String[] args) {
+        // Please write a Java program which declares, assign values of each
+        // primitive data type and print out in console the result
+        byte grade = 10;
+        short year = 2022;
+        int number = 724578137;
+        long largeNumber = 1234567891011121314L;
+        char letter = 'A';
+        float smallDecimal = 3.14f;
+        double largeDecimal = 3.14159265359D;
+        boolean isTrue = true;
+        // printing the values on console
+        System.out.println("byte datatype and its value: " +grade);
+        System.out.println("short datatype and its value: " +year);
+        System.out.println("integer datatype and its value: " +number);
+        System.out.println("long datatype and its value: " +largeNumber);
+        System.out.println("character datatype and its value: " +letter);
+        System.out.println("float datatype and its value: " +smallDecimal);
+        System.out.println("double datatype and its value: " +largeDecimal);
+        System.out.println("boolean datatype and its value: " +isTrue);
+
+    }
+}
---
+++ Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Please write a Java program which declares, assign values of each
+    // primitive data type and print out in console the result
+    byte grade;
+    short year;
+    int number;
+    long largeNumber;
+    char letter;
+    float smallDecimal;
+    double largeDecimal;
+    boolean isTrue;
+        grade = 10;
+        year = 2022;
+        number = 724578137;
+        largeNumber = 1246898987L;
+        letter = 'A';
+        smallDecimal = 3.14f;
+        largeDecimal = 3.14159265359D;
+        isTrue = true;
+            System.out.println("byte datatype and its value: " +grade);
+            System.out.println("short datatype and its value: " +year);
+            System.out.println("integer datatype and its value: " +number);
+            System.out.println("long datatype and its value: " +largeNumber);
+            System.out.println("character datatype and its value: " +letter);
+            System.out.println("float datatype and its value: " +smallDecimal);
+            System.out.println("double datatype and its value: " +largeDecimal);
+            System.out.println("boolean datatype and its value: " +isTrue);
+
+        }
+}
